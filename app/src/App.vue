43fa27190dff21d0a9d0a1302951583ac0ed8d34
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-06-21 23:29:51 +0530

    Add search field in v-app-bar.

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,36 +5,18 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-row>
+        <v-col cols="9">
+          <v-text-field
+            filled
+            dense
+            outlined
+            label="Search TeeRex"
+            prepend-inner-icon="mdi-magnify"
+            class="mt-6"
+          ></v-text-field>
+        </v-col>
+      </v-row>
     </v-app-bar>
 
     <v-main>
@@ -58,3 +40,6 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss" scoped>
+</style>
