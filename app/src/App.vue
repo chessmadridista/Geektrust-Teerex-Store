bdce4cbd8162e5b4a775390d612e95422c6c4f89
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-06-22 19:21:38 +0530

    Add icons to the top navigation header.

--- app/src/App.vue
+++ app/src/App.vue
@@ -6,15 +6,22 @@
       dark
     >
       <v-row>
-        <v-col cols="9">
+        <v-col cols="8" lg="10" class="mt-7">
           <v-text-field
             filled
             dense
             outlined
             label="Search TeeRex"
             prepend-inner-icon="mdi-magnify"
-            class="mt-6"
           ></v-text-field>
+        </v-col>
+        <v-col cols="4" lg="2" class="mt-6">
+          <v-btn icon>
+            <v-icon>mdi-filter</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-cart-outline</v-icon>
+          </v-btn>
         </v-col>
       </v-row>
     </v-app-bar>
