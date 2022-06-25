96e9da0db98344c24fd7b8d9826a2a26e50257e9
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-06-25 18:14:57 +0530

    Specify column size for each item and use fetch api to get json data.

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,6 +1,22 @@
 <template>
   <v-container>
-    
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-img></v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-img></v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-img></v-img>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -10,6 +26,14 @@
 
     data: function () {
       return {
+        items: fetch('https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json')
+              .then(response => response.json())
+              .then(data => {
+                  this.items = data
+                  
+                  return this.items;
+                  }
+              ),
       }
     }
   }
