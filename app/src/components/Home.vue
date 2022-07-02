4846c4e18cfd07f6758914a65d6e918a258a2ca4
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-07-02 18:58:22 +0530

    Add rating and price of items.

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -6,15 +6,44 @@
       :key="item.id" 
       cols="12" 
       lg="4"
-      align="center">
+      align="center"
+      class="mt-lg-16">
         <v-card 
-        height="300px" 
+        height="375px" 
         width="300px" 
         align="center">
           <v-img
           :src="item.imageURL"
           width="250px"
           height="250px"></v-img>
+          <v-row>
+            <v-col cols="8">
+              <v-card-title>{{ item.name }}</v-card-title>
+              <v-card-text>
+                <v-row class="ml-0">
+                    <v-rating
+                    :value="4.5"
+                    color="amber"
+                    dense
+                    half-increments
+                    readonly
+                    size="14"></v-rating>
+                    <!-- <v-btn
+                    color="primary">Add</v-btn> -->
+                </v-row>
+                <v-row class="ml-1 mt-4">
+                    <span>${{ item.price }}</span>
+                </v-row>
+              </v-card-text>
+            </v-col>
+            <v-col cols="2" class="mt-10">
+              <v-btn
+              color="primary"
+              fab>
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
     </v-row>
