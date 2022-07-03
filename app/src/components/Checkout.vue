8e26ccc21639a1a9a89545a9db6edc84bf99296f
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-07-03 11:54:38 +0530

    Create checkout component.

---
+++ app/src/components/Checkout.vue
@@ -0,0 +1,72 @@
+<template>
+      <v-container>
+        <v-row>
+        <v-col 
+        v-for="item in items" 
+        :key="item.id" 
+        cols="12"
+        md="6"
+        lg="4"
+        align="center"
+        class="my-md-8">
+          <v-card 
+          height="375px" 
+          width="300px" 
+          align="center">
+            <v-img
+            :src="item.imageURL"
+            width="250px"
+            height="250px"></v-img>
+            <v-row>
+              <v-col cols="8">
+                <v-card-title>{{ item.name }}</v-card-title>
+                <v-card-text>
+                  <v-row class="ml-0">
+                      <v-rating
+                      :value="4.5"
+                      color="amber"
+                      dense
+                      half-increments
+                      readonly
+                      size="14"></v-rating>
+                      <!-- <v-btn
+                      color="primary">Add</v-btn> -->
+                  </v-row>
+                  <v-row class="ml-1 mt-4">
+                      <span>${{ item.price }}</span>
+                  </v-row>
+                </v-card-text>
+              </v-col>
+              <v-col cols="2" class="mt-10">
+                <v-btn
+                color="error"
+                fab>
+                  <v-icon>mdi-minus</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Checkout',
+  data: function () {
+    return {
+      items: [],
+    }
+  },
+  beforeCreate() {
+    this.items = fetch("https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json")
+        .then((response) => response.json())
+        .then((data) => {
+          this.items = data;
+          return true;
+        })
+    console.log("'Items' has been initialized.");
+  }
+}
+</script>
--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router';
 import Home from './components/Home.vue'
+import Checkout from './components/Checkout.vue'
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false
@@ -12,6 +13,11 @@ const router = new VueRouter({
       name: 'Home',
       path: '/',
       component: Home
+    },
+    { 
+      name: 'Checkout',
+      path: '/checkout',
+      component: Checkout
     }
   ]
 });
